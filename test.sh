b04ecd61045bd68aaade0a827385939c662e4bc1
rawrex <rawrex@proton.me>
2026-03-25 12:53:27 +0700

    Manual tests test data variant

--- test.sh
+++ test.sh
@@ -12,6 +12,7 @@ touch .repeat &&\
 git add .srs/index.txt .repeat &&\
 git commit -m 'Inited srs' &&\
 echo 'Added SRS to git' &&\
+# cp -rv ../test_data_more/* . &&\
 cp -rv ../test_data/* . &&\
 cp -rv ../config.json .srs/ &&\
 git add -A &&\
