aae2a6ea88668c8607cc9c0abbd09459a538fab1
rawrex <rawrex@proton.me>
2026-04-15 15:06:09 +0700

    Minor code reordering

--- core/cards_manager.py
+++ core/cards_manager.py
@@ -48,11 +48,13 @@ class CardsManager:
         note_to_claimed_lines: dict[str, set[int]] = {}
         for entry in index_entries:
             card = self.factory.make_card(index_entry=entry)
-            view = card.context_view()
             cards.append(card)
+
+            view = card.context_view()
             line_range = (entry.start_line, entry.end_line)
             context_blocks.setdefault(entry.note_abs_path, {})
             context_blocks[entry.note_abs_path][line_range] = view.primary_block().text
+
             note_to_claimed_lines.setdefault(entry.note_abs_path, set())
             note_to_claimed_lines[entry.note_abs_path].update(range(entry.start_line, entry.end_line + 1))
 
